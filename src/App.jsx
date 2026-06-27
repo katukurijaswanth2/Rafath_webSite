07cdcc042d65390bc93c61dc65432ae000fe9825
katukuri_jaswanth <katukurijaswanth2@gmail.com>
2026-06-27 23:44:48 +0530

    categories

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import MenuCategories from './components/categories/MenuCategories'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>hello
+
+    </h1>
+    <MenuCategories />
+    </>
+  )
+}
+
+export default App
